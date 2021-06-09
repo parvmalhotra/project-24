5ae09c7c8ec3a52b0019b5f4541d56e63d8eb70c
parvmalhotra <76687368+parvmalhotra@users.noreply.github.com>
2021-06-09 16:52:39 +0530

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,33 @@
+class Iron{
+	constructor(x,y,width,height)
+   	{ var options = {
+     restitution : 0.8,
+	 friction:0.9,
+	 density:30
+
+
+	   }
+	// assign options to the rubber ball
+		this.x=x;
+		this.y=y;
+		this.width=width;
+        this.height=height;
+		this.body=Bodies.rectangle(this.x, this.y, this.width,this.height, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var rubberpos=this.body.position;		
+			push()
+			translate(rubberpos.x, rubberpos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("black");
+	        rect(0,0,this.width,this.height);
+
+			pop()
+	}
+
+}
